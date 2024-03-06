0bdce8f48081a7f8a7ef949233c55a8fca11e026
venkat <pvenkatesh566@gmail.com>
2024-03-06 13:23:02 +0530

    added logs

--- demo/src/main/java/com/sample/v3/demo/resource/DemoResource.java
+++ demo/src/main/java/com/sample/v3/demo/resource/DemoResource.java
@@ -1,14 +1,19 @@
 package com.sample.v3.demo.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/demo")
+
 public class DemoResource {
+    Logger logger= LoggerFactory.getLogger(DemoResource.class);
     @GetMapping("hello")
     public String hello(){
+        logger.info("Hello World");
         return "Hello World";
     }
 }
